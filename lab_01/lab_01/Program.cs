3e072ba5d20860d35643bf0ebaca0a751838be10
unknown <Patryk.Adamczyk@microsoft.wsei.edu.pl>
2022-03-10 09:22:45 +0100

    druga wersja

--- lab_01/lab_01/Program.cs
+++ lab_01/lab_01/Program.cs
@@ -8,5 +8,22 @@ namespace lab_01
         {
             Console.WriteLine("Hello World!");
         }
+        public class Ulamek
+        {
+            private static int licznik = 1;
+           private static mianownik = 2;
+
+            
+                
+                public Ulamek() 
+            {
+                this.Ulamek;
+            }
+
+            public Ulamek(int licznik)
+            {
+                this.licznik = name;
+            }
+        }
     }
 }
