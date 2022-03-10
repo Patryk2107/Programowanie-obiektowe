5a09c9dd6e000305e231c39e2b2c23b82af80ccb
unknown <Patryk.Adamczyk@microsoft.wsei.edu.pl>
2022-03-10 09:48:47 +0100

    lekcja

--- lab_01/lab_01/Program.cs
+++ lab_01/lab_01/Program.cs
@@ -4,25 +4,59 @@ namespace lab_01
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Uczen[] uczniowie = new Uczen[]
+            {
+                new Uczen("Staszek")
+            }
         }
         public class Ulamek
         {
             private static int licznik = 1;
-           private static mianownik = 2;
+            private static mianownik = 2;
 
             
                 
-                public Ulamek() 
+            public Ulamek() 
             {
-                this.Ulamek;
+                this.licznik;
+                this mianownik;
             }
 
-            public Ulamek(int licznik)
+            public Ulamek(int licznik, int mianownik)
             {
-                this.licznik = name;
+                this.licznik = licznik;
+                this.mianownik = mianownik;
+            }
+
+            public Ulamek(int licznik, int mianownik)
+            {
+                this.licznik = licznik;
+                this.mianownik = mianownik;
+            }
+
+            public Ulamek(Ulamek licznik, Ulamek mianownik)
+            {
+                this.licznik = Ulamek.licznik;
+                this.mianownik = Ulamek.mianownik;
+            }
+
+
+
+            
+
+        }
+        public class Uczen
+        {
+            private string imie;
+            public Uczen(string Uczen)
+            {
+                this imie = imie;
+            }
+            public override string ToString()
+            {
+                return $"Student: {this.imie}";
             }
         }
     }
